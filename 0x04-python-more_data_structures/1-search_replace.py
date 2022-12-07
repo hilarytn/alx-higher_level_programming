30a0904b7c4b80954805a407788503b47a932be8
Hilary Titus Naor <hilarytn@gmail.com>
2022-12-07 07:28:39 -0800

    Search and replace in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,9 +6,3 @@ def search_replace(my_list, search, replace):
         if new_list[x] == search:
             new_list[x] = replace
     return new_list
-"""
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-newlist = search_replace(my_list, 2, 89)
-
-print(newlist)
-print(my_list)"""
