317620622afc7230f2ed05a47bd0cd3817430eea
Hilary Titus Naor <hilarytn@gmail.com>
2022-12-07 12:52:10 -0800

    Convert Roma Numeral to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    total = 0
-    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-        for roman in reversed(roman_string):
-            arabic = digits[roman]
-            total += arabic if total < arabic * 5 else -arabic
-     return total
+
+    roman_letters = [
+            ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
+            ['X', 10], ['V', 5], ['I', 1]
+    ]
+    num = 0
+    last = 0
+
+    for letter in roman_string:
+        for elem in roman_letters:
+            if letter == elem[0]:
+                if last == 0 or last >= elem[1]:
+                    num += elem[1]
+                elif last < elem[1]:
+                    num += elem[1] - (last * 2)
+                    last = elem[1]                                                                                          return num
