a7eefa82486263bd3d3083b6ac707ca65cc6721c
Hilary Titus Naor <hilarytn@gmail.com>
2022-12-21 03:44:02 -0800

    Define a square by coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,11 +39,11 @@ class Square:
     def area(self):
         return (self.__size * self.__size)                                                                                                                                  
     """size"""
-    @size.sette
+    @size.setter
     def size(self, value):
-    self.__size = value                                                                                                     if type(self.__size) != int:
-        raise TypeError("size must be an integer")                                                                          if self.__size < 0:
-        raise ValueError("size must be >= 0")                                                                                                                                                                                                       """print the square"""
+        self.__size = value                                                                                                     if type(self.__size) != int:
+            raise TypeError("size must be an integer")                                                                          if self.__size < 0:
+            raise ValueError("size must be >= 0")                                                                                                                                                                                                   """print the square"""
     def my_print(self):
         if self.size == 0:
             print("")
