5f8ab5215418c2eb0ad77bb2cdbcef778d7856c9
Hilary Titus Naor <hilarytn@gmail.com>
2022-12-21 01:59:31 -0800

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isintance(value, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
