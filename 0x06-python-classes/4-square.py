a4a6a0c9584f3295bab5cd08f016cd9ce35bbb66
Hilary Titus Naor <hilarytn@gmail.com>
2022-12-21 01:28:05 -0800

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 class Square:
-    """Square Class
-     A Square Class
-    """
-    
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """__init__
-        The __init__ method initializes the size value of the square.
-        
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-        Raises:
-            TypeError: If `size` type is not `int`.
-            ValueError: If `size` is less than `0`.
-        """ 
-
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
+        """Inituialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
-                                                                                                                            @property
-    def size(self):                                                                                                             return self.__size
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.size)
 
-                                                                                                                            @size.setter
-    def size(self, size):
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
+    @size.setter
+    def size(self, value):
+        if not isintance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-                                                                                                                                if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
-
-                                                                                                                            def area(self):
-        """Returns the current square area                                                                                                                                                                                                              """
-                                                                                                                               return self.__size ** 2
+    def area(self):
+        """Return teh current area of the square."""
+        return (self.__size * self.__size)
