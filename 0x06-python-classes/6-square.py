16c066899c6c20cd2217461f215bd85d3e6c8bf3
Hilary Titus Naor <hilarytn@gmail.com>
2022-12-21 03:39:45 -0800

    Define a square by coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,53 +1,57 @@
 #!/usr/bin/python3
-# 6-square.py
-"""Define a class Square."""
+"""create square"""
+
 
 class Square:
-    """Represent a square."""
-
+    """inicialize size"""
     def __init__(self, size=0, position=(0, 0)):
-        """initialize a new square.
-        Args:
-            size (int): The size of the new square.
-            position (int, int): TRhe position of the new square.
-        """
         self.size = size
         self.position = position
 
+    """return size"""
     @property
     def size(self):
-        """get/set teh current size of the square."""
         return (self.__size)
 
+    """return position"""
+    @property
+    def position(self):
+        return (self.__position)
+
+    """handle errors"""
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    @property
+    
+    """position errors"""
+    @position.setter
     def position(self, value):
-        if (not isinstance (value, tuple) or 
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        if type(value) != tuple or len(value) != 2 or type(value[0]) != int \
+                or type(value[1]) != int or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
+    """return the square"""
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self>__size)
-
+        return (self.__size * self.__size)                                                                                                                                  
+    """size"""
+    @size.sette
+    def size(self, value):
+    self.__size = value                                                                                                     if type(self.__size) != int:
+        raise TypeError("size must be an integer")                                                                          if self.__size < 0:
+        raise ValueError("size must be >= 0")                                                                                                                                                                                                       """print the square"""
     def my_print(self):
-        """Print teh square with the # character."""
-        if self.___size == 0:
+        if self.size == 0:
             print("")
-            return 
-
-        [print("") for i in range(0, self.__position[1]]
-            for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print('#', end="") for k in range(0, self.__size)]
+        else:
+            for i in range(self.position[1]):
+                print("")
+            for row in range(self.size):
+                print(" " * self.position[0], end="")
+                for col in range(self.size):
+                    print("#", end="")
                 print("")
