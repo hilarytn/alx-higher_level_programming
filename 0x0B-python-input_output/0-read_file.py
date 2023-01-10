9c381cd787c78853383a4314dff105cb26360eea
Hilary Titus Naor <hilarytn@gmail.com>
2023-01-10 06:38:30 -0800

    Read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """
+ """
         A function that reads a text file (UTF8) and prints it to stdout
     Args:
         filename: name of the file
     Returns:
         Nothing
     """
+def read_file(filename=""):
     with open('workfile', encoding="utf-8") as f:
         for line in f:
             print(line, end="")
