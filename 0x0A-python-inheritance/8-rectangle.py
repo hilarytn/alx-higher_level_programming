7ca6d51fd40fc9827f549eec24694c2a034cd726
Hilary Titus Naor <hilarytn@gmail.com>
2023-01-11 14:39:04 -0800

    Inherit basegeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,7 +3,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """ Class that defines a rectangle from BaseGeometry Class """
     def __init__(self, width, height):
+        """Initializa instance"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
