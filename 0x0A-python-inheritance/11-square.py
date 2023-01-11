8549a49bdbc163bccf8b0207aa414f1e51288e0e
Hilary Titus Naor <hilarytn@gmail.com>
2023-01-11 15:20:56 -0800

    Instantiate square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,17 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
 """Import modules"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Define instance of rectangle as Square"""
+    """ Class that defines a Square from Rectangle class """
     def __init__(self, size):
-        """Initialize the instance of Rectangle"""
-        self.integer_validator(size, size)
-        super().__init__(size, size)
-        self.__size = size
+	""" Method that initializes a Square """
+	self.integer_validator("size", size)
+	self.__size = size
+	super().__init__(self.__size, self.__size)
+
+    def area(self):
+	""" Method that returns a string with the area """
+	return super().area()
 
     def __str__(self):
-        """String method for strings"""
-        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+	""" Special method that returns a printable string """
+	return "[Square] {}/{}".format(self.__size, self.__size)
