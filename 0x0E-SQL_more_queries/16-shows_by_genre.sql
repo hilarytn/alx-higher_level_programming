e360bb4a90826713a30b322825381ee17cef3aa7
Hilary Titus Naor <hilarytn@gmail.com>
2023-02-15 14:56:41 -0800

    List all shows and all genres linked

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,8 @@
--- Lists all Comedy shows
--- in the database `hbtn_0d_tvshows`.
-
-SELECT tv_shows.title
+-- Script that lists all shows and all genres
+SELECT tv_shows.title, tv_genres.name -- Query to join tables
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+     LEFT JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     LEFT JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
