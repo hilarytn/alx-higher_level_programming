591b2f6bde9a93e1d94fb16a03f956f6ccd2b1a9
Hilary Titus Naor <hilarytn@gmail.com>
2023-02-15 14:16:48 -0800

    List all shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
-- Lists all shows contained in `hbtn_0d_tvshows`
+-- Lists all shows contained in `hbtn_0d_tvshows`
 -- that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
