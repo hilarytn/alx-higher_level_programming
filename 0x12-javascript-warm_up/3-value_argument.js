8c2c4c094765bc300bfafe00f4d6dcc31713287b
Hilary Titus Naor <hilarytn@gmail.com>
2023-03-13 08:21:24 -0700

    semistandard fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,11 @@
 
 process = require('process');
 
-if (process.argv[2] === undefined)
-  console.log('No argument')
+if (process.argv[2] === undefined) 
+{
+  console.log('No argument');
+}
 else
+{
   console.log(process.argv[2]);
+}
