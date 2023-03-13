641938a462231ab8cb90bc234c279b40ed4e72a0
Hilary Titus Naor <hilarytn@gmail.com>
2023-03-13 16:00:53 -0700

    Fix initialization

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
-if (process.argv[2] === undefined) { console.log('Missing number of occurrences'); } else {
-  for (let i = 0; i <= process.argv[2]; i++) {
+if (process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 1; i <= process.argv[2]; i++) {
     console.log('C is fun');
   }
 }
