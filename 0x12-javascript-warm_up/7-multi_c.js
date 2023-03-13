07755a3da5da43605d74f4fb7fb6b4af5505cff7
Hilary Titus Naor <hilarytn@gmail.com>
2023-03-13 15:54:52 -0700

    Print x number of times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const process = require('process');
+if (process.argv[2] === undefined) { console.log('Missing number of occurrences'); } else {
+  for (let i = 0; i <= process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+}
