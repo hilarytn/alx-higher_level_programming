4bddab4d5af795eb5bfcd77a12e320fa1843f5f5
Hilary Titus Naor <hilarytn@gmail.com>
2023-03-13 15:29:46 -0700

    Semistandard fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
 
-const process = require('process')
+const process = require('process');
 
-if (process.argv[2] === undefined) 
-  console.log('Not a number');
-else if (typeof(process.argv[2]) === 'string'){
-  if ((parseInt(process.argv[2])))
-    console.log(`My number: ${parseInt(process.argv[2])}`);
-  else
-    console.log('Not a number');
-}
-else
-  console.log(`My number: ${parseInt(process.argv[2])}`);
+if (process.argv[2] === undefined) { console.log('Not a number'); } else if (typeof (process.argv[2]) === 'string') {
+  if ((parseInt(process.argv[2]))) { console.log(`My number: ${parseInt(process.argv[2])}`); } else { console.log('Not a number'); }
+} else { console.log(`My number: ${parseInt(process.argv[2])}`); }
