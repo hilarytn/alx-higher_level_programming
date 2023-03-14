a9e8a3a6f137f4af388567f567ab48c70aab5730
Hilary Titus Naor <hilarytn@gmail.com>
2023-03-14 00:43:06 -0700

    Fix braces

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,5 +7,7 @@ if (parseInt(process.argv[2])) {
   for (let i = 1; i <= process.argv[2]; i++) {
     str += 'x';
   }
-  for (let j = 1; j <= process.argv[2]; j++) { console.log(str); }
+  for (let j = 1; j <= process.argv[2]; j++) {
+    console.log(str);
+  }
 } else { console.log('Missing size'); }
