8ba08a4c1b3c3f294fdd611495ce960290646e73
Hilary Titus Naor <hilarytn@gmail.com>
2023-03-14 00:34:14 -0700

    Print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+if (parseInt(process.argv[2])) {
+  let str = '';
+  for (let i = 1; i <= process.argv[2]; i++) {
+    str += 'x';
+  }
+  for (let j = 1; j <= process.argv[2]; j++) { console.log(str); }
+} else { console.log('Missing size'); }
