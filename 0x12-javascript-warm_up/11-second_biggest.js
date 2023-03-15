db75274b4ef04af3c55d2a1c53c2862e9015ebf1
Hilary Titus Naor <hilarytn@gmail.com>
2023-03-15 09:02:58 -0700

    Standard fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
 
 const process = require('process');
-let arg = process.argv;
+const arg = process.argv;
 
-if (!arg[2] || arg.length === 3)
-        console.log(0);
-else {
-        let arr = [];
-        let p = 0;
-        for (let j = 2; j < arg.length; j++) {
-                arr[p] = parseInt(arg[j])
-                p++;
-        }
-        arr.sort();
-        console.log(arr[arr.length - 2]);
+if (!arg[2] || arg.length === 3) { console.log(0); } else {
+  const arr = [];
+  let p = 0;
+  for (let j = 2; j < arg.length; j++) {
+    arr[p] = parseInt(arg[j]);
+    p++;
+  }
+  arr.sort();
+  console.log(arr[arr.length - 2]);
 }
-
