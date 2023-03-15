443571b34ba2f341895c5574a301882dc59f98ee
Hilary Titus Naor <hilarytn@gmail.com>
2023-03-15 06:10:01 -0700

    Obtain factorial recursively

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const process = require('process');
+let arg = process.argv[2];
+let temp = 1;
+
+function factorial () {
+  if (!parseInt(arg) || arg === 0) { console.log(1); } else {
+    temp = temp * arg;
+    arg--;
+    if (arg > 1) { factorial(arg); } else { console.log(temp); }
+    return 0;
+  }
+}
+
+factorial(arg);
